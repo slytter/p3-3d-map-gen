bbcdff51e31939f9446ed4be6b75eeac1051859a
nikolajsc <schildt.nikolaj@gmail.com>
2016-10-11 13:02:03 +0200

    penis?

---
+++ Vertexmodify/Assets/cameraScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+
+    public RawImage rawimage;
+    public Color[,] newImage;
+    public texture
+
+}
+
+    void Start () {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        rawimage.texture = webcamTexture;
+        rawimage.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+        
+        
+    }
+	
+	
+	void Update () {
+	
+	}
+
+    void Snapshot()
+{
+        new
+
+}
+
+
+}
