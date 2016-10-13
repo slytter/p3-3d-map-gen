adaff5798249296abfd523b150a6896e5de50ae0
Blodbad123 <sdpo15@student.aau.dk>
2016-10-13 13:04:09 +0200

    Man kan nu detecte grøn, gul, orange, og rød. Der er lidt problemer med en overloaded method vi gerne vil have samlet i den samme function(rød) Der mangler også noget image optimization og at alle funktioner kan køre på samme tid

--- Vertexmodify/Assets/Scripts/ColorDetection.cs
+++ Vertexmodify/Assets/Scripts/ColorDetection.cs
@@ -5,13 +5,16 @@ using System;
 
 public class ColorDetection : MonoBehaviour
 {
-	Texture2D originalIm;
-	Texture2D newIm;
-	Color[] pixN;
-	Color[] pixO;
 
+	Color[] pixO;
 	void Start ()
 	{
+
+		Texture2D originalIm;
+		Texture2D newIm;
+		Color[] pixN;
+		 
+
 		newIm = new Texture2D (512, 512);
 		originalIm = (Texture2D)GetComponent<Renderer> ().material.mainTexture;
 		GetComponent<Renderer> ().material.mainTexture = newIm;
@@ -21,50 +24,98 @@ public class ColorDetection : MonoBehaviour
 		newIm.SetPixels (pixN);
 		newIm.Apply ();	
 
-		//TxtBox = GameObject.Find ("Txt");
+		// looking for green
+		//colorDetection (pixN, newIm, 0.42f, 0.24f, 0.20f, 0.5f);
+
+		// Loking for yellow
+		//colorDetection (pixN, newIm, 0.23f, 0.15f, 0.25f, 0.5f);
+
+		// overloaded method for red, since intervals is crossing 0
+		colorDetection (pixN, newIm, 1f, 0f, 0.94f, 0.02f, 0.25f, 0.5f);
+
+		// Looking for Orange
+		//colorDetection (pixN, newIm, 0.14f, 0.06f, 0.25f, 0.5f);
 	}
 
 	void Update ()
 	{
-
-		if (Input.GetKeyDown (KeyCode.F1))
-			colorDetection ();
-		
+			
 	}
 
-	void updateOldImAndApplyNewIm ()
+	bool [,] colorDetection (Color [] pixN, Texture2D newIm, float hueMax, float hueMin, float sat, float val)
 	{
 
-		newIm.SetPixels (pixN);
-		newIm.Apply ();	
-		pixO = newIm.GetPixels ();
-	}
-
-	bool [,] colorDetection ()
-	{
-		
 		//Float 2d array to assign and return
-		bool [,] pixelPos = new bool[512, 512];  
+		bool[,] pixelPosGreen = new bool[512, 512];  
 		float h; 
 		float s; 
 		float v; 
 
-		for (int y = 1; y < newIm.height - 1; y++) {
-			for (int x = 1; x < newIm.width - 1; x++) {
+		for (int y = 0; y < newIm.height; y++) {
+			for (int x = 0; x < newIm.width; x++) {
 
 
 				//Converting rgb to HSV and assigning float variables. 
-				Color.RGBToHSV (pixO [y * newIm.width + x], out h, out s, out v); 
+				Color.RGBToHSV (pixN [y * newIm.width + x], out h, out s, out v); 
 
 				// Looking for green
-				if (h > 0.194f  && h < 0.42f && s > 0.3f && v > 0.6f) {
+				if (h > hueMin && h < hueMax && s > sat && v > val) {
 					// Assigning position x,y in float array. 
-					pixelPos [x, y] = true;
+					pixelPosGreen [x, y] = true;
 				} else {
-					pixelPos [x, y] = false; 
+					pixelPosGreen [x, y] = false; 
 				}
 			}
 		} 
-		return pixelPos;
+		printBinary (pixelPosGreen, pixN, newIm); 
+		return pixelPosGreen;
+	}
+	bool [,] colorDetection (Color [] pixN, Texture2D newIm, float Max,float Min, float hueMax, float hueMin, float sat, float val)
+	{
+
+		//Float 2d array to assign and return
+		bool[,] pixelPosGreen = new bool[512, 512];  
+		float h; 
+		float s; 
+		float v; 
+
+		for (int y = 0; y < newIm.height; y++) {
+			for (int x = 0; x < newIm.width; x++) {
+
+
+				//Converting rgb to HSV and assigning float variables. 
+				Color.RGBToHSV (pixN [y * newIm.width + x], out h, out s, out v); 
+
+				// Looking for green
+				if (h > Min && h < hueMin && s > sat && v > val || h > hueMax && h < Max && s > sat && v > val) {
+					// Assigning position x,y in float array. 
+					pixelPosGreen [x, y] = true;
+				} else {
+					pixelPosGreen [x, y] = false; 
+				}
+			}
+		} 
+		printBinary (pixelPosGreen, pixN, newIm); 
+		return pixelPosGreen;
+	}
+
+
+	void printBinary (bool[,] input, Color [] pixN, Texture2D newIm)
+	{
+
+		for (int y = 0; y < input.GetLength(0); y++) {
+			for (int x = 0; x < input.GetLength(1); x++) {
+
+
+				if (input [x, y] == true) {
+					pixN [y * newIm.width + x] = new Color (0, 1, 0); 
+				} else {
+					pixN [y * newIm.width + x] = new Color (0, 0, 0);
+				}
+			}
+		}
+
+		newIm.SetPixels (pixN);
+		newIm.Apply ();	
 	}
 }
