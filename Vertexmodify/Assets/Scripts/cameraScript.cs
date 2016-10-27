b091737b43eadca9246bd793966bd601d9fda8d6
Blodbad123 <sdpo15@student.aau.dk>
2016-10-27 13:36:37 +0200

    webcam og snap funktion virker nu

--- Vertexmodify/Assets/Scripts/cameraScript.cs
+++ Vertexmodify/Assets/Scripts/cameraScript.cs
@@ -4,43 +4,79 @@ using System.Collections;
 
 public class cameraScript : MonoBehaviour {
 
+	private string savePath = "/Users/ejer/Desktop/Medialogy/3. Semester/P3/P3-3DMapGen/Vertexmodify/CamSnaps/"; 
+
+	int _CaptureCounter = 0; 
 	public GameObject targetPlane; 
 
     public RawImage rawimage;
-    //public Color[] newImage;
+	Color [] pix; 
 	Texture2D tex1; 
+	Texture2D original;
 	WebCamTexture webcamTexture; 
 
 
 
 	void Start () {
-		webcamTexture = new WebCamTexture();
-		tex1 = new Texture2D (webcamTexture.width,webcamTexture.height);
-        rawimage.texture = webcamTexture;
-        rawimage.material.mainTexture = webcamTexture;
-        webcamTexture.Play();
 
-        
-        
+		StartCoroutine (fixBug ());    
     }
 	
-	
+
 	void Update () {
-	
+
+		//tex1.SetPixels (webcamTexture.GetPixels ()); 
+		//tex1.Apply (); 
+
 		if (Input.GetKeyDown ("s")) {
-			Snapshot (); 
+			Snapshot();
 		}
+	}
+
+	void Snapshot()
+{
+
+		tex1.SetPixels(webcamTexture.GetPixels()); 
+		tex1.Apply ();
+		targetPlane.GetComponent<Renderer> ().material.mainTexture = tex1; 
+		webcamTexture.Play (); 
+
+		System.IO.File.WriteAllBytes(savePath + _CaptureCounter.ToString() + ".png", tex1.EncodeToPNG());
+		//webcamTexture.Pause (); 
+		_CaptureCounter ++; 
 
 	}
 
-    void Snapshot()
-{
-		//newImage = webcamTexture.GetPixels(); 
-		tex1.SetPixels (webcamTexture.GetPixels()); 
-		tex1.Apply (); 
-		targetPlane.GetComponent<Renderer> ().material.mainTexture = tex1; 
-}
+	IEnumerator fixBug(){
+
+		webcamTexture = new WebCamTexture();
+		webcamTexture.Play();
+
+		if (webcamTexture.width <= 16 || webcamTexture.height <= 16) {
+			while (!webcamTexture.didUpdateThisFrame) {
+				yield return new WaitForEndOfFrame (); 
+
+			}
+
+			webcamTexture.Pause (); 
+			Color32[] colors = webcamTexture.GetPixels32 (); 
+			webcamTexture.Stop (); 
+
+			yield return new WaitForEndOfFrame ();
+
+			webcamTexture.Play (); 
+
+		}
+
+		rawimage.texture = webcamTexture;
+		rawimage.material.mainTexture = webcamTexture;
+
+		rawimage.enabled = false; 
 
 
+		tex1 = new Texture2D (webcamTexture.width,webcamTexture.height);
 
+
+		Debug.Log ("The height of the webcam is: " + webcamTexture.height + " The width of the webcam is: " + webcamTexture.width); 
+	}
 }
