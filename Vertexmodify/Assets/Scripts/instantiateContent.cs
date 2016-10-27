e39aee45549fb47d053d57fd84713f93ba7c71c0
Spejderman <r.odgaard@gmail.com>
2016-10-27 15:48:11 +0200

    beginning of instantiate

---
+++ Vertexmodify/Assets/Scripts/instantiateContent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class instantiateContent : MonoBehaviour {
+
+	public GameObject trees[];
+
+	void Start () {
+		trees = new GameObject[16];
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
