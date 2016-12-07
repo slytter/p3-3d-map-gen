bce9e8e387d566d0fbc843cb19f3463af53ab9b8
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2016-12-07 13:39:29 +0100

    add calibration scene

--- Vertexmodify/Assets/Scripts/cameraScript.cs
+++ Vertexmodify/Assets/Scripts/cameraScript.cs
@@ -3,100 +3,119 @@ using UnityEngine.UI;
 using System.Collections;
 using System;
 
-public class cameraScript : MonoBehaviour {
+public class cameraScript : MonoBehaviour
+{
 
-    //private string savePath = "/Users/ejer/Desktop/Medialogy/3. Semester/P3/P3-3DMapGen/Vertexmodify/Assets/WebcamBilleder"; 
-    private string savePath;
-    public GameObject targetPlane; 
-    public RawImage rawimage;
-	protected Color [] pixFromSnap; 
+	//private string savePath = "/Users/ejer/Desktop/Medialogy/3. Semester/P3/P3-3DMapGen/Vertexmodify/Assets/WebcamBilleder";
+	private string savePath;
+	public GameObject targetPlane;
+	public RawImage rawimage;
+	protected Color[] pixFromSnap;
 	Texture2D originalImageFromWebcam;
 	Texture2D flippedImageFromCamera;
-	WebCamTexture webcamTexture; 
+	WebCamTexture webcamTexture;
 
 	public Texture2D outputImage;
-    string currentImageName;
+	string currentImageName;
 
 	gameState gameState;
 
-	public GameObject chooseButton, retakeButton, snapButton;
+	public GameObject chooseButton, retakeButton, snapButton, calibrateButton;
 
-	void Start () {
+	void Start ()
+	{
 
 		savePath = Application.dataPath + "/Resources/WebcamBilleder/";
-        print(savePath);
+		print (savePath);
 		StartCoroutine (initWebcam ()); 
 
 		chooseButton.SetActive (false);
 		retakeButton.SetActive (false);
+		calibrateButton.SetActive (false);
 		snapButton.SetActive (true);
-    }
 
-	public void Snapshot() {
+	}
 
-		originalImageFromWebcam.SetPixels(webcamTexture.GetPixels()); 
+	public void Snapshot ()
+	{
+
+		originalImageFromWebcam.SetPixels (webcamTexture.GetPixels ()); 
 		originalImageFromWebcam.Apply ();
 		targetPlane.GetComponent<Renderer> ().material.mainTexture = originalImageFromWebcam; 
 		rawimage.enabled = false;
 
-		currentImageName = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss") + ".png";
+		currentImageName = System.DateTime.Now.ToString ("dd-MM-yyyy-HH-mm-ss") + ".png";
 
 		flippedImageFromCamera = flipXAndY (originalImageFromWebcam);
 
-		System.IO.File.WriteAllBytes(savePath + currentImageName, flippedImageFromCamera.EncodeToPNG());
+		System.IO.File.WriteAllBytes (savePath + currentImageName, flippedImageFromCamera.EncodeToPNG ());
 
 	    
 //		pixFromSnap = originalImageFromWebcam.GetPixels (); 
 //		outputImage = originalImageFromWebcam;
-		originalImageFromWebcam = new Texture2D (webcamTexture.width,webcamTexture.height);
+		originalImageFromWebcam = new Texture2D (webcamTexture.width, webcamTexture.height);
 
 		chooseButton.SetActive (true);
 		retakeButton.SetActive (true);
+		calibrateButton.SetActive (true);
 		snapButton.SetActive (false);
 
 	}
 
 
-	public void retake(){
+	public void retake ()
+	{
 		rawimage.enabled = true; 
 		webcamTexture.Play ();
 		chooseButton.SetActive (false);
 		retakeButton.SetActive (false);
 		snapButton.SetActive (true);
-    }
+	}
 
 
-    public void choose() {
-		gameState = GameObject.Find("gameState").GetComponent<gameState>();
-        gameState.chosenImage = currentImageName;
-        print("image chosen: " + currentImageName);
-        Application.LoadLevel("sagen");
-    }
+	public void choose ()
+	{
+		gameState = GameObject.Find ("gameState").GetComponent<gameState> ();
+		gameState.chosenImage = currentImageName;
+		print ("image chosen: " + currentImageName);
+		Application.LoadLevel ("sagen");
+	}
 
 	public Texture2D flipXAndY (Texture2D original)
 	{
 		TimingModule.timer ("flipX&Y", "start");
-		Texture2D flipped = new Texture2D(original.height,original.width);
+		Texture2D flipped = new Texture2D (original.height, original.width);
 
-		int xN = original.width -1;
-		int yN = original.height -1;
+		int xN = original.width - 1;
+		int yN = original.height - 1;
 
 
-		for(int i=0;i<xN;i++){
-			for(int j=0;j<yN;j++){
-				flipped.SetPixel(yN - j, xN-i, original.GetPixel(i,j));
+		for (int i = 0; i < xN; i++) {
+			for (int j = 0; j < yN; j++) {
+				flipped.SetPixel (yN - j, xN - i, original.GetPixel (i, j));
 
 			}
 		}
-		flipped.Apply();
+		flipped.Apply ();
 		TimingModule.timer ("flipX&Y", "end");
 
 		return flipped;
 	}
 
-	IEnumerator initWebcam(){
-		webcamTexture = new WebCamTexture();
-		webcamTexture.Play();
+
+	public void calibrate ()
+	{
+		gameState = GameObject.Find ("gameState").GetComponent<gameState> ();
+		gameState.chosenImage = currentImageName;
+		print ("image chosen: " + currentImageName);
+		Application.LoadLevel ("calibrate");
+	}
+
+
+	IEnumerator initWebcam ()
+	{
+		webcamTexture = new WebCamTexture ();
+		webcamTexture.Play ();
 
 		if (webcamTexture.width <= 16 || webcamTexture.height <= 16) {		
 			while (!webcamTexture.didUpdateThisFrame) {
@@ -116,7 +135,7 @@ public class cameraScript : MonoBehaviour {
 		rawimage.texture = webcamTexture;
 		//rawimage.material.mainTexture = webcamTexture;
 
-		originalImageFromWebcam = new Texture2D (webcamTexture.width,webcamTexture.height);
+		originalImageFromWebcam = new Texture2D (webcamTexture.width, webcamTexture.height);
 
 		Debug.Log ("The height of the webcam is: " + webcamTexture.height + " The width of the webcam is: " + webcamTexture.width); 
 	}
--- Vertexmodify/Assets/Scripts/gameState.cs
+++ Vertexmodify/Assets/Scripts/gameState.cs
@@ -3,17 +3,23 @@ using UnityEngine.UI;
 using System.Collections;
 using System;
 
-public class gameState : MonoBehaviour {
+public class gameState : MonoBehaviour
+{
 
-	void Awake(){
-		Debug.Log("gamestate init");
+	void Awake ()
+	{
+		Debug.Log ("gamestate init");
 		GameObject.DontDestroyOnLoad (this.gameObject);
 	}
-    
+
 	public Texture2D image;
 
 	public string test = "yo";
-    public string chosenImage = "";
+	public string chosenImage = "";
 
-
+	public string[] color = new string[4];
+	public float[] hueMin = new float[4];
+	public float[] hueMax = new float[4];
+	public float[] sat = new float[4];
+	public float[] val = new float[4];
 }
